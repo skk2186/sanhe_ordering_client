b1a90285f904c505b9f293ebfa858cee18e8da75
shaokaiaaa666 <1085134460@qq.com>
2026-09-09 20:40:32 +0800

    chore: adapt code for local machine environment

--- vite.config.js
+++ vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/app-api': {
-        target: 'http://127.0.0.1:48081',
+        target: process.env.VITE_API_PROXY_TARGET || 'http://127.0.0.1:48081',
         changeOrigin: true,
         ws: true,
         secure: false,
